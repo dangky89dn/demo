294f15c9c7a7544842b19543e514310f9fd29fe3
dangky89dn <139837492+dangky89dn@users.noreply.github.com>
2025-01-05 07:13:27 +0700

    Update CollectionExample1.java
    dá123

--- 19JSP02/src/jsp/examples/collections/CollectionExample1.java
+++ 19JSP02/src/jsp/examples/collections/CollectionExample1.java
@@ -19,7 +19,7 @@ public class CollectionExample1 {
 	public static void main(String[] args) {
         List<String> arrayList = new ArrayList<String>();
         arrayList.add("PHP1");
-        arrayList.add("123Pyt11hon1");
+        arrayList.add("123Py123t11hon1");
         arrayList.add("Java1111111");
         arrayList.add("C++112132323");
         System.out.println("Các phần tử của ArrayList");
